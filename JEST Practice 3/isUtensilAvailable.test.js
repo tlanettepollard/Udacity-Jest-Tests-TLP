38e8335b067680f84b437b36862174dcdb17c755
T. Lanette <TLPollard4516@gmail.com>
2022-07-11 20:56:08 -0400

    Added second function & test to Jest 3
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ JEST Practice 3/isUtensilAvailable.test.js
@@ -0,0 +1,17 @@
+// isUtensilAvailable.test.js
+
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+var isUtensilAvailable = require('./isUtensilAvailable');
+
+describe('isUtensilAvailable', () => {
+    it('will return true if the utensil is found', async() => {
+        var utensil = 'fork';
+        var result = await isUtensilAvailable(utensil);
+        expect(result).toEqual(true);
+    })
+    
+    it('will return an error if the utensil is not found', async () => {
+        var invalidUtensil = 'tree';
+        await expect(isUtensilAvailable(invalidUtensil)).rejects.toEqual('No utensils found.');
+    });
+})
