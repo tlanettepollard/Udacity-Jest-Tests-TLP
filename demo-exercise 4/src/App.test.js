a33e603de7cdcb1b2ae47e48e01c879710b12e9d
T. Lanette <TLPollard4516@gmail.com>
2022-08-03 18:06:41 -0400

    Added test but failed
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- demo-exercise 4/src/App.test.js
+++ demo-exercise 4/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import * as React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+	it('will have expected fields', () => {
+		var view = render(<App />);
+
+		var labels = view.screen.getAllByText(/name/);
+		expect(labels.length).toEqual(2);
+
+		var firstNameInput = view.screen.getByTestId('first-name-input');
+		var lastNameInput = view.screen.getByTestId('last-name-input');
+		expect(firstNameInput).toBeInTheDocument();
+		expect(lastNameInput).toBeInTheDocument();
+
+		var submitButton = view.screen.getByText('Submit');
+		expect(submitButton).toBeInTheDocument();
+	});
 });
